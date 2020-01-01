1432915db327ba898504e94d50f3b273bd0d4643
Shai <ifryed@gmail.com>
2020-01-02 01:14:36 +0200

    SQL demo
    create, add, extract and delete

---
+++ t_9_sql/src/com/ariel/oop/Main.java
@@ -0,0 +1,98 @@
+package com.ariel.oop;
+
+import java.sql.*;
+
+/**
+ * @author sqlitetutorial.net
+ */
+public class Main {
+    /**
+     * Connect to a sample database
+     */
+    public static Connection connect(String db_name) {
+        Connection conn = null;
+        try {
+            // db parameters
+            db_name = db_name.endsWith(".db") ? db_name : db_name + ".db";
+            String url = "jdbc:sqlite:sqlite/db/" + db_name;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return conn;
+    }
+
+    /**
+     * Connect to a sample database
+     *
+     * @param sql_conn the database file name
+     */
+    public static void createNewDatabase(Connection sql_conn) {
+        try {
+            if (sql_conn != null) {
+                DatabaseMetaData meta = null;
+                meta = sql_conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createNewTable(Connection sql_conn) {
+        String sql = "CREATE TABLE IF NOT EXISTS ships(\n"
+                + "    id integer PRIMARY KEY,\n"
+                + "    name text NOT NULL,\n"
+                + "    type text,\n"
+                + "    class text\n"
+                + ");";
+
+        try (Statement stmt = sql_conn.createStatement()
+        ) {
+            stmt.execute(sql);
+
+            System.out.println("New Table added successfully to the StarFleet Database");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String database_name = "StarFleet";
+        // Part 1: Basics
+        Connection conn = connect(database_name);
+
+        createNewDatabase(conn);
+        createNewTable(conn);
+
+        // Part 2: Insert, extract and delete data
+//        SqlHandler sql_h = new SqlHandler(database_name);
+//
+//        sql_h.printShips();
+//
+//        sql_h.insertShipData(
+//                "USS Enterprise",
+//                "Warp 5 prototype/long range cruiser",
+//                "Naval Experimental (NX)"
+//                );
+//
+//        sql_h.insertShipData(
+//                "USS Ares",
+//                "Battle Cruiser",
+//                "Ares Class prototype"
+//        );
+//
+//        sql_h.deleteShipById(3);
+//
+//        sql_h.printShips();
+    }
+}
---
+++ t_9_sql/src/com/ariel/oop/SqlHandler.java
@@ -0,0 +1,133 @@
+package com.ariel.oop;
+
+import java.sql.*;
+
+public class SqlHandler {
+    String mDB_path;
+    Connection mConn;
+
+    SqlHandler(String db_path) {
+        mDB_path = db_path;
+        mConn = connect2DB(mDB_path);
+    }
+
+    /**
+     * Connects to the SQL database
+     * @param db_path the path to the database
+     * @return The connection object
+     */
+    public Connection connect2DB(String db_path) {
+        if (!(null == mConn)) {
+            try {
+                mConn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.err.println("Could not close old connection");
+                return null;
+            }
+        }
+        Connection conn = null;
+        try {
+            // db parameters
+            db_path = db_path.endsWith(".db") ? db_path : db_path + ".db";
+            String url = "jdbc:sqlite:sqlite/db/" + db_path;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return conn;
+    }
+
+    /**
+     * Get the max ship id
+     */
+    public int getLastId() {
+        String sql = "SELECT MAX(id) "
+                + "FROM ships";
+
+        try (PreparedStatement pstmt = mConn.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+
+            // loop through the result set
+            int ret_id = 0;
+            while (rs.next()) {
+                ret_id = rs.getInt(1);
+                return ret_id;
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return -1;
+    }
+
+    /**
+     * Prints all the ships
+     */
+    public void printShips() {
+        String sql = "SELECT name,id,type,class "
+                + "FROM ships";
+
+        try (PreparedStatement pstmt = mConn.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(
+                        "Id:\t"+rs.getInt(2)
+                        +"\n\tName:\t"+rs.getString(1)
+                        +"\n\tType:\t"+rs.getString(3)
+                        +"\n\tClass:\t"+rs.getString(4)
+                );
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Adds another ship to the database
+     * @param name Ship name
+     * @param type Ship type
+     * @param class_str Ship class
+     */
+    public void insertShipData(String name, String type, String class_str) {
+        String sql = "INSERT INTO ships(name,id,type,class) VALUES(?,?,?,?)";
+        int next_id = getLastId() + 1;
+        try (PreparedStatement pstmt = mConn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setInt(2, next_id);
+            pstmt.setString(3, type);
+            pstmt.setString(4, class_str);
+            pstmt.executeUpdate();
+
+            System.out.println("Added new ship.");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Deletes ship by ID
+     * @param ship_id Ships ID
+     */
+    public void deleteShipById(int ship_id){
+        String sql = "DELETE FROM ships WHERE id = ?";
+
+        try (PreparedStatement pstmt = mConn.prepareStatement(sql)) {
+
+            // set the corresponding param
+            pstmt.setInt(1, ship_id);
+            // execute the delete statement
+            pstmt.executeUpdate();
+
+            System.out.println("Ship retired");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
